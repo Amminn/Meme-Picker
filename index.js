c57404eb62a6230cef34f4d5b9498f07561ae12e
Amminn <vrb.nnn@gmail.com>
2022-10-01 20:03:36 +0100

    feat: :art: working on moods rendering function

--- index.js
+++ index.js
@@ -204,4 +204,47 @@ function getEmotionsArray(cats) {
   return result
 }
 
-console.log(getEmotionsArray(catsData))
+// -------------------------------------------------
+
+/*
+Challenge:
+1. Have the new function "renderEmotionsRadios" 
+   take in a single parameter. Name that parameter
+   "cats". 
+2. Inside renderEmotionsRadios, set up a const called 
+   "emotions" and set it equals to whatever is returned 
+   by calling getEmotionsArray, passing in "cats" as an 
+   argument.
+3. For now, renderEmotionsRadios should just log out 
+   "emotions".
+4. Call renderEmotionsRadios passing in catsData.
+*/
+
+function renderEmotionsRadios(cats) {
+  const emotions = getEmotionsArray(cats)
+  let moods = [...new Set(emotions)]
+  moods.map(item => {
+    console.log(item)
+  })
+  moods.map(item => {
+    return console.log(`<input type="radio" id="${item}" name="${item}" value="${item}">
+    <label for="${item}">${item}</label>`)
+  })
+}
+
+// renderEmotionsRadios(catsData)
+
+/*
+Challenge:
+1. Take control of the 'emotion-radios' div.
+2. In renderEmotionsRadios, set up a let 
+   to hold our string of HTML. You can initialise
+   it with an empty string.
+3. Iterate over "emotions" and put each emotion 
+   in a <p> tag and then add them to the let you 
+   created in step 2. 
+4. Render the string to the 'emotion-radios' div.
+*/ 
+
+const emotionRadio = document.getElementById('emotion-radios')
+emotionRadio.innerHTML = renderEmotionsRadios(catsData)
